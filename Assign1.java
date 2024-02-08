d7acce37f0caf1eee6912cbb3964c765995dfccb
StephenMonk84 <57074825+StephenMonk84@users.noreply.github.com>
2024-02-08 12:01:09 -0800

    Add files via upload
    Added input validation for characters, and looped to make sure that valid input is provided by the user.

--- Assign1.java
+++ Assign1.java
@@ -9,7 +9,7 @@ class Assign1{
         // 1. This checks the text and removes punctuation and new lines
         newInp = checkInput(inpString);
         String newInpL = newInp.toLowerCase();
-        char chFreq, chFreqL;
+        char chFreq;
         //2. This first line calculates the total length of the string using builtin String functions
         System.out.println("The total length of the string is: "+ newInp.length() + " characters.");
         //3. This call counts the words
@@ -18,13 +18,14 @@ class Assign1{
         System.out.println("The most common character is " + mostCommonChar(newInp)+ ".");
         //5. This line searches for a specific character and is case insensitive
         System.out.println("Please enter a character to search for: ");
-        chFreq=inp.nextLine().charAt(0);
-        chFreqL = Character.toLowerCase(chFreq);
-        System.out.println("The character " + chFreq + " appears " + charFrequency(newInpL, chFreqL) + " times as both upper and lowercase.");
+        String charSearch = inp.nextLine();
+        chFreq=checkChar(charSearch);
+        System.out.println("The character " + chFreq + " appears " + charFrequency(newInpL, chFreq) + " times as both upper and lowercase.");
         //6. This searches for a specfic word and is case insensitive.
         System.out.println("Please enter a word to search for: ");
         String inpSearch = inp.nextLine();
-        System.out.println("The word " + inpSearch + " appears " + wordFrequency(newInpL, inpSearch) + " times.");
+        String inpValid = checkInput(inpSearch);
+        System.out.println("The word " + inpSearch + " appears " + wordFrequency(newInpL, inpValid) + " times.");
        //7.  This counts the amount of unique words  case insensitive.
         System.out.println("The number of unique words is " + noOfUniqueWords(newInpL));
         inp.close();
@@ -32,10 +33,18 @@ class Assign1{
     }
 
     public static String checkInput(String test){
+        /** This method validates string inputs and prompts the user if the input is empty or null
+         * This method also removes any special characters and punctuation like periods and commas
+         */
         Scanner inpScan = new Scanner(System.in);
-        if (test == null || test ==""){
-            System.out.println("Please enter a string with characters. ");
-            test = inpScan.nextLine();
+        while(true){
+            if (test.equals(null) || test.equals("") || test.equals(" ")){
+                System.out.println("Please enter a string with characters. ");
+                test = inpScan.nextLine();
+            }
+            else{
+                break;
+            }
         }
         String temp = test.replaceAll("[\n\r]", " ");
         String s = temp.replaceAll("[\\.,]", "");
@@ -43,12 +52,14 @@ class Assign1{
     }
 
     public static int wordCount(String inp){
+        /**This method counts the number of words by searching for whitespaces and splitting the string */
         int count = 0;
         count = inp.split(" ").length;
         return count;
     }
 
     public static int charFrequency(String inp, char ch){
+        /** This method counts the frequenct of the selected character this includes whitespace */
         int count = 0;
         for (int i =0; i < inp.length();i++){
             if(inp.charAt(i) == ch){
@@ -58,7 +69,9 @@ class Assign1{
         return count;
 
     }
+
     public static int wordFrequency(String temp, String word){
+        /**This method counts the number of instances that a word appears this is case insensitive */
         int count = 0;
 
         String wordArr[] = temp.split(" ");
@@ -72,6 +85,7 @@ class Assign1{
     }
 
     public static int noOfUniqueWords(String str){
+        /** This method counts the number of unique words in the string, this method uses a List of Strings and sorts them and then counts whenever a new word is encountered */
         int count =1;
         String nextWord;
         String words[] = str.split(" ");
@@ -92,6 +106,7 @@ class Assign1{
     }
 
     public static String mostCommonChar(String inp){
+        /**This method counts the most common character by removing whitespace and then counting each letter and then comparing with the next letter */
         String str = "";
         String temp = inp.replaceAll("[\\s]", "");
         String arrayStr[] = temp.split("");
@@ -112,6 +127,24 @@ class Assign1{
         }
         
         return str;
+    }
+
+    public static char checkChar(String str){
+        /**This method checks for null or empty input then sends back a valid character for searching */
+        Scanner inp = new Scanner(System.in);
+        char chTemp, ch;
+        while(true){
+            if(str.isEmpty() || str.equals("") || str.equals(" ")){
+                System.out.println("Please enter a character to search for: ");
+                str = inp.nextLine();
+                chTemp=str.charAt(0);
+                ch = Character.toLowerCase(chTemp);
+            } else{
+                break;
+            }
+        }
+        ch = str.charAt(0);
+        return ch;
     }
 
 }
