19b9bb272b5497269fa1484b73764898b3571c64
StephenMonk84 <57074825+StephenMonk84@users.noreply.github.com>
2024-02-21 13:07:21 -0800

    Added two working versions of week 3 assignments.
    Clock Three is the best version of the software.

---
+++ ClockTwo.java
@@ -0,0 +1,42 @@
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+
+public class ClockTwo {
+    public static void main(String[] args){
+        final DateTimeFormatter CUS_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss dd-MM-yyyy");
+        final int WAIT_TIME = 1000;
+        
+        Runnable timeObj = () -> {
+            LocalDateTime timer;
+            while(true){
+                timer = LocalDateTime.now();
+            }
+            
+        };
+
+        Runnable displayObj = () ->{
+            LocalDateTime ldt;
+            while(true){
+               ldt = LocalDateTime.now();
+                String currentTime = ldt.format(CUS_FORMATTER);  
+                System.out.println(" Current Time: " + currentTime);
+            try{Thread.sleep(WAIT_TIME);} catch(InterruptedException e){ e.printStackTrace();}
+            } 
+        };
+
+        Thread t1 = new Thread(timeObj);
+        Thread t2 = new Thread(displayObj);
+        t2.setPriority(Thread.MAX_PRIORITY);
+
+        t1.start();
+        t2.start();
+
+
+    }
+
+
+
+    
+}
