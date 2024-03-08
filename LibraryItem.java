89e4b6d5486921cf4503b19b2965eebf8656b80f
StephenMonk84 <57074825+StephenMonk84@users.noreply.github.com>
2024-03-08 14:41:10 -0800

    Added First File for Week 6 Homework
    Added first generic class for week 6 work.

---
+++ LibraryItem.java
@@ -0,0 +1,45 @@
+public class LibraryItem <T, V>{
+    private T itemID;
+    private V title;
+    private V author;
+
+    
+        
+    public LibraryItem(T itemID, V title, V author){
+        this.itemID = itemID;
+        this.title = title;
+        this.author = author;
+    }
+
+    public void setItemID(T itemID){
+        this.itemID = itemID;
+    }
+
+    public void setTitle(V title){
+        this.title = title;
+    }
+
+    public void setAuthor(V author){
+        this.author = author;
+    }
+
+    public <T>T getItemID(){
+        return itemID;
+    }
+
+    public <V>V getTitle(){
+        return title;
+    }
+
+    public <V>V getAuthor(){
+        return author;
+    }
+
+
+
+    public void printerInfo(){
+        System.out.println("The id of the book is " + itemID + " the title is " + title + " and the author is " + author);
+    }
+
+    
+}
